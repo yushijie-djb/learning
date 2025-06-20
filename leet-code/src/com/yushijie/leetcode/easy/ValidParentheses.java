b1ec387be6dbf65a5c9150b6fa413dbdb630e852
yushijie <yu15549580451@163.com>
2025-06-20 11:30:26 +0800

    '合法的括号'

---
+++ leet-code/src/com/yushijie/leetcode/easy/ValidParentheses.java
@@ -0,0 +1,55 @@
+package com.yushijie.leetcode.easy;
+/**
+ * @description
+ * @author yushijie
+ * @date 2025/6/10 11:18
+ * @version 1.0
+ */
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 每个右括号都有一个对应的相同类型的左括号。
+ */
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 == 1) {
+            return false;
+        }
+
+        Map<Character, Character> pairs = new HashMap<Character, Character>() {{
+            put(')', '(');
+            put(']', '[');
+            put('}', '{');
+        }};
+        Deque<Character> stack = new LinkedList<Character>();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (pairs.containsKey(ch)) {
+                if (stack.isEmpty() || !stack.peek().equals(pairs.get(ch))) {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
